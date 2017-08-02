1a8bdc8fa0a34c04c089701b491ddd32a5744761
Juscostajr <juscostajr@gmail.com>
2017-08-02 20:42:29 -0300

    Alteração para olá juscelino

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Olá mundo!!!");
+  res.send("Olá Juscelino JR.");
 });
 
 module.exports = router;
