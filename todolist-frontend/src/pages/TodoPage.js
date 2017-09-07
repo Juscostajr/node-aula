c250e1aec4ba0dd237177460e42afc68e25f0371
Juscostajr <juscostajr@gmail.com>
2017-09-06 21:38:44 -0300

    torar o pau

--- todolist-frontend/src/pages/TodoPage.js
+++ todolist-frontend/src/pages/TodoPage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+
 import axios from 'axios';
+
+import {
+    Table, Button,
+    Modal, ButtonGroup
+} from 'react-bootstrap';
+
 class TodoPage extends Component {
-    
+
     state = {
         todos: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('http://localhost:3001/todos')
             .then((response) => {
                 console.log(response);
@@ -15,18 +22,60 @@ class TodoPage extends Component {
                 });
             }).catch((error) => {
                 console.error(error);
-            });
+            })
+    }
+
+    onExcluirClick = (todo) => {
+        window.confirm("Deseja excluir a tarefa " + todo.id + "?");
+    }
+
+    renderTodo = () => {
+        const todos = this.state.todos;
+
+        const todosComponents = todos.map((todo, index) => {
+            return (
+                <tr>
+                    <td>{todo.id}</td>
+                    <td>{todo.title}</td>
+                    <td>{todo.date}</td>
+                    <td>{todo.completed}</td>
+                    <td>
+                        <ButtonGroup bsSize="small">
+                            <Button bsStyle="warning">Editar</Button>
+                            <Button bsStyle="danger"
+                                onClick={() => this.onExcluirClick(todo)}>
+                                Excluir
+                            </Button>
+                        </ButtonGroup>
+                    </td>
+                </tr>
+            );
+        });
+
+        return todosComponents;
     }
 
     render() {
         const todos = this.state.todos;
-        const todosComponents = todos.map((todo,index) => {
-            return <p>{todo.id} - {todo.title}</p>;
-        })
-        return(
-          <section>
-              <h1>Página de Tarefas</h1>
-          </section>  
+
+        return (
+            <section>
+                <h1>Página de Tarefas</h1>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Título</th>
+                            <th>Data</th>
+                            <th>Concluída</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderTodo()}
+                    </tbody>
+                </Table>
+            </section>
         );
     }
 }
