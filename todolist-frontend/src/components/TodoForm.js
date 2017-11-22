efa96da0362b9e4107eab14ee1048ec81e70345a
Juscostajr <juscostajr@gmail.com>
2017-11-22 21:44:07 -0200

    Arquivos que faltaram

---
+++ todolist-frontend/src/components/TodoForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+import { Button, Modal, FormGroup, FormControl, ControlLabel,
+} from 'react-bootstrap';
+
+class TodoForm extends Component {
+
+    state = {};
+
+    componentWillReceiveProps(nextProps) {
+        const selectedTodo = nextProps.selectedTodo;
+        this.setState({
+            id: selectedTodo.id,
+            title: selectedTodo.title,
+            description: selectedTodo.description,
+        });
+    }
+
+    onTitleChange = (event) => {
+        this.setState({ title: event.target.value });
+    }
+
+    onDescriptionChange = (event) => {
+        this.setState({ description: event.target.value });
+    }
+
+    onSave = () => {
+        const { id, title, description } = this.state;
+
+        if (!title || !description) {
+            alert('Preencha o titulo e a descrição da tarefa.');
+            return;
+        }
+
+        this.props.onSave(id, title, description);
+    }
+
+    render() {
+        const { showForm, onClose } = this.props;
+
+        const { id, title, description } = this.state;
+        return (
+            <Modal show={showForm} onHide={onClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Tarefa</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <FormGroup>
+                            <ControlLabel>#</ControlLabel>
+                            <FormControl value={id} disabled />
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>Título</ControlLabel>
+                            <FormControl value={title} onChange={this.onTitleChange} />
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>Descrição</ControlLabel>
+                            <FormControl componentClass="textarea"
+                                value={description} onChange={this.onDescriptionChange} />
+                        </FormGroup>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button bsSize="small" onClick={onClose}>Cancelar</Button>
+
+                    <Button bsSize="small" bsStyle="info"
+                        onClick={this.onSave}>Salvar</Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+}
+
+export default TodoForm;
---
+++ todolist-frontend/src/components/TodoTable.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+import {
+    Table, Button,
+    ButtonGroup, ButtonToolbar,
+    ToggleButtonGroup, ToggleButton,
+} from 'react-bootstrap';
+
+import moment from 'moment';
+
+class TodoTable extends Component {
+
+    renderTodo = () => {
+        const { todos, onEditarClick, onExcluirClick, onConcluidaChange } = this.props;
+
+        const todosComponents = todos.map((todo, index) => {
+            return (
+                <tr>
+                    <td>{todo.id}</td>
+                    <td>{todo.title}</td>
+                    <td>{moment(todo.creation_date).format('DD/MM/YYYY [às] HH:mm')}</td>
+                    <td>
+                        <ButtonToolbar>
+                            <ToggleButtonGroup bsSize="small" type="radio" onChange={(concluida) => onConcluidaChange(todo.id, concluida)} name="completed" value={todo.completed}>
+                                <ToggleButton value={true}>Concluída</ToggleButton>
+                                <ToggleButton value={false}>Pendente</ToggleButton>
+                            </ToggleButtonGroup>
+                        </ButtonToolbar>
+                    </td>
+                    <td>
+                        <ButtonGroup bsSize="small">
+                            <Button bsStyle="warning"
+                                onClick={() => onEditarClick(todo)}>
+                                Editar
+                            </Button>
+                            <Button bsStyle="danger"
+                                onClick={() => onExcluirClick(todo)}>
+                                Excluir
+                            </Button>
+
+                        </ButtonGroup>
+                    </td>
+                </tr>
+            );
+        });
+
+        return todosComponents;
+    }
+
+    render() {
+        return (
+            <Table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Título</th>
+                        <th>Data</th>
+                        <th>Concluída</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderTodo()}
+                </tbody>
+            </Table>
+        )
+    }
+}
+
+export default TodoTable;
